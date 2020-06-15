29b57aed4ec1cc5623a47effdc3452692c6f74c5
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2020-06-15 08:49:59 +0530

    date time

---
+++ 1.py
@@ -0,0 +1,8 @@
+from time import time, ctime
+ 
+prev_time = ""
+while True:
+    the_time = ctime(time())
+    if prev_time != the_time:
+        print "The time is:", ctime(time())
+        prev_time = the_time
